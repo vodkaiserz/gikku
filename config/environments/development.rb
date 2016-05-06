dc567202ce6fc91d4ba53ebbe2c04f16123c7a9a
vodkaiserz <vodkaiserz@gmail.com>
2016-05-06 09:24:59 +0700

    biar ga susa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.web_console.whitelisted_ips = '10.0.2.2'
+  config.web_console.whiny_requests = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
