12e98ee670648903a2acfb15605e88414793d80c
vodkaiserz <vodkaiserz@gmail.com>
2016-05-06 11:45:02 +0700

    imageupload

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -36,3 +36,8 @@ Monologue.config do |config|
   config.show_rss_icon = true
 
 end
+
+Monologue.config do |monologue|
+  monologue.image_upload.max_picture_size = 2.megabytes
+  monologue.image_upload.upload_path = "monologue"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506015822) do
+ActiveRecord::Schema.define(version: 20160506043805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
   create_table "monologue_posts", force: :cascade do |t|
     t.boolean  "published"
@@ -51,32 +67,6 @@ ActiveRecord::Schema.define(version: 20160506015822) do
     t.datetime "updated_at"
   end
 
-  create_table "wellspring_entries", force: :cascade do |t|
-    t.string   "type"
-    t.string   "title"
-    t.string   "slug"
-    t.json     "payload"
-    t.integer  "user_id"
-    t.string   "author_name"
-    t.datetime "published_at"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
 
-  add_index "wellspring_entries", ["slug"], name: "index_wellspring_entries_on_slug", using: :btree
-  add_index "wellspring_entries", ["type"], name: "index_wellspring_entries_on_type", using: :btree
-  add_index "wellspring_entries", ["user_id"], name: "index_wellspring_entries_on_user_id", using: :btree
-
-  create_table "wellspring_entry_search_data", force: :cascade do |t|
-    t.integer  "entry_id"
-    t.string   "attr_name"
-    t.tsvector "search_data"
-    t.text     "raw_data"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "wellspring_entry_search_data", ["entry_id"], name: "index_wellspring_entry_search_data_on_entry_id", using: :btree
-  add_index "wellspring_entry_search_data", ["search_data"], name: "idx_search_data", using: :gin
 
 end
